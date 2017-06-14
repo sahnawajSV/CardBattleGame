b08801018b0575869723225e0bd68dbcc66e7b21
Vishal Aggarwal <vishal.aggarwal@softvision.com>
2017-06-14 18:47:49 +0530

    Initial Controller + ViewModel Files and Folder Structure added

---
+++ CardBattleGame/ViewControllers/EditDeckViewController/EditDeckViewModel/EditDeckViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EditDeckViewModel.swift
+//  CardBattleGame
+//
+//  Created by Vishal Aggarwal on 14/06/17.
+//  Copyright © 2017 SAHNAWAJ BISWAS. All rights reserved.
+//
+
+import UIKit
+
+class EditDeckViewModel: NSObject {
+
+}
