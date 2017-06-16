ea414845fc87632c2c90523ecc55ef26ba43a355
SAHNAWAJ BISWAS <md.sahnawajbiswas@softvision.com>
2017-06-16 16:10:08 +0530

    Added Common Error Handler function : Which is responsible for handling all Application Level errors and display Alert with custom error message.

--- CardBattleGame/Utility/CBGErrorHandler.swift
+++ CardBattleGame/Utility/CBGErrorHandler.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
+enum ErrorTpe: Error {
+    case failedRequest
+    case faildParseWeatherData
+    case invalidResponse
+    case unknown
+}
+
 class CBGErrorHandler: Error {
     
+   static func handle( error : ErrorTpe)  {
+        switch error {
+        case .failedRequest:
+            errorAlert(errorTitle: "Error", errorMsg: "Failed Request")
+        case .faildParseWeatherData:
+            errorAlert(errorTitle: "Error", errorMsg: "Faild To Parse Weather Data")
+        case .invalidResponse:
+            errorAlert(errorTitle: "Error", errorMsg: "Invalid Response")
+        default:
+            errorAlert(errorTitle: "Error", errorMsg: "Unknown")
+        }
+    }
+    
+    
+    static private func errorAlert(errorTitle: String, errorMsg: String){
+        
+        let alertController =  UIAlertController(title: errorTitle, message: errorMsg, preferredStyle: UIAlertControllerStyle.alert)
+        let okAction  = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action) in
+            
+        }
+        
+        alertController.addAction(okAction)
+        
+        
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindowLevelAlert + 1;
+        alertWindow.makeKeyAndVisible()
+        
+        alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
+
+    }
 }
