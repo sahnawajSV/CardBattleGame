0e1adbe6240487620fc2375eefbe7d1b28152a8d
SAHNAWAJ BISWAS <md.sahnawajbiswas@softvision.com>
2017-06-20 13:01:57 +0530

    Added EditDeckCollectionViewCell

---
+++ CardBattleGame/ViewControllers/EditDeckViewController/View/EditDeckCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EditDeckCollectionViewCell.swift
+//  CardBattleGame
+//
+//  Created by SAHNAWAJ BISWAS on 20/06/17.
+//  Copyright © 2017 SAHNAWAJ BISWAS. All rights reserved.
+//
+
+import UIKit
+
+class EditDeckCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var healthLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var battlePointLbl: UILabel!
+    @IBOutlet weak var addDeckButton: UIButton!
+
+}
