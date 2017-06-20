b22b0ba2757c9babca3b0e5a57ebe4da1e392fe4
SAHNAWAJ BISWAS <md.sahnawajbiswas@softvision.com>
2017-06-20 12:30:31 +0530

    Changed to Singleton Class. Change Card object as bailable initialiser.

--- CardBattleGame/ServiceManager/DataManager/CardListDataSource.swift
+++ CardBattleGame/ServiceManager/DataManager/CardListDataSource.swift
@@ -10,43 +10,52 @@ import UIKit
 
 class CardListDataSource: NSObject {
     
+    static let sharedInstance: CardListDataSource = {
+        let instance = CardListDataSource()
+        return instance
+    }()
     
+    // MARK: - Properties
+    //
+    var cardList:[Card] = []
+    
+    
+    // MARK: - Initialization
+    //
     override init() {
         super.init()
         populateData()
     }
     
-    var cardList:[Card] = []
-    
     
     // MARK:- Populate Data from plist
+    //
     func populateData() {
         if let path = Bundle.main.path(forResource: "CardList", ofType: "plist") {
             if let dictArray = NSArray(contentsOfFile: path) {
                 for item in dictArray {
                     if let dict = item as? NSDictionary {
-                        let name = dict["name"] as! String
-                        let id = dict["id"] as! String
-                        let attack = dict["attack"] as! String
-                        let battlepoint = dict["battlepoint"] as! String
-                        let health = dict["health"] as! String
-                        
-                        let cardData = Card(name: name, id: id, attack: attack, battlepoint: battlepoint, health: health)
-                        cardList.append(cardData)
+                        let cardData = Card(dictionary: dict)
+                        if let cardData = cardData {
+                            cardList.append(data)
+                        }
                     }
                 }
             }
         }
     }
     
+    
     // MARK:-Return Cards
+    //
     func  fetchCardList() -> [Card] {
         return cardList
     }
     
+    
     // MARK : - Get Number Of Cards
+    //
     func numbeOfCards() -> Int {
-        
         return cardList.count
     }
 }
