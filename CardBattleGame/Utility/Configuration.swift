ae3b034703ceff85e338a73a0f8bfafb742803a1
SAHNAWAJ BISWAS <md.sahnawajbiswas@softvision.com>
2017-06-20 12:32:12 +0530

    shift API content to Connection Manager, and Lat Lon to Location Manager

--- CardBattleGame/Utility/Configuration.swift
+++ CardBattleGame/Utility/Configuration.swift
@@ -12,18 +12,5 @@ import Foundation
 
 struct Defaults {
     
-    static let Latitude: Double = 51.400592
-    static let Longitude: Double = 4.760970
-    static let DISTANCE_FILTER = 5000.0
 }
 
-struct API {
-    
-    static let APIKey = "9035e8294254f66c2a5e636c76907571"
-    static let BaseURL = URL(string: "https://api.darksky.net/forecast/")!
-    
-    static var AuthenticatedBaseURL: URL {
-        return BaseURL.appendingPathComponent(APIKey)
-    }
-    
-}
