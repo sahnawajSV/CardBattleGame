cb7dbe39b7f105002408e85ca7f751e3c6ecd183
SAHNAWAJ BISWAS <md.sahnawajbiswas@softvision.com>
2017-06-20 17:55:52 +0530

    Initialise default Value

--- CardBattleGame/Model/Card.swift
+++ CardBattleGame/Model/Card.swift
@@ -8,34 +8,32 @@
 import Foundation
 
 class Card: NSObject {
+  
+  var name: String = ""
+  var id: Int16 = 0
+  var attack: Int16 = 0
+  var battlepoint: Int16 = 0
+  var health: Int16 = 0
+  
+  
+  init?(dictionary: NSDictionary) {
     
-    var name:String?
-    var id:String?
-    var attack:String?
-    var battlepoint:String?
-    var health:String?
-    
-
-    init?(dictionary: NSDictionary) {
-        
-        guard let name = dictionary["name"] as? String,
-            let id = dictionary["id"] as? String,
-            let attack = dictionary["attack"] as? String,
-            let battlepoint = dictionary["battlepoint"] as? String,
-            let health: String = dictionary["health"] as? String else {
-                return nil
-        }
-        
-        
-        self.name = name
-        self.id = id
-        self.attack = attack
-        self.battlepoint = battlepoint
-        self.health = health
-        
-        
-        super.init()
-
+    guard let name = dictionary["name"] as? String,
+      let id = Int16((dictionary["id"] as? String)!),
+      let attack = Int16((dictionary["attack"] as? String)!),
+      let battlepoint = Int16((dictionary["battlepoint"] as? String)!),
+      let health = Int16((dictionary["health"] as? String)!) else {
+        return nil
     }
-
+    
+    self.name = name
+    self.id = id
+    self.attack = attack
+    self.battlepoint = battlepoint
+    self.health = health
+    
+    super.init()
+    
+  }
+  
 }
--- CardBattleGame/ServiceManager/DataManager/CardListDataSource.swift
+++ CardBattleGame/ServiceManager/DataManager/CardListDataSource.swift
@@ -9,52 +9,52 @@
 import UIKit
 
 class CardListDataSource: NSObject {
-    
-    static let sharedInstance: CardListDataSource = {
-        let instance = CardListDataSource()
-        return instance
-    }()
-    
-    // MARK: - Properties
-    //
-    var cardList:[Card] = []
-    
-    
-    // MARK: - Initialization
-    //
-    override init() {
-        super.init()
-        populateData()
-    }
-    
-    
-    // MARK:- Populate Data from plist
-    //
-    func populateData() {
-        if let path = Bundle.main.path(forResource: "CardList", ofType: "plist") {
-            if let dictArray = NSArray(contentsOfFile: path) {
-                for item in dictArray {
-                    if let dict = item as? NSDictionary {
-                        if let data = Card(dictionary: dict) {
-                            cardList.append(data)
-                        }
-                    }
-                }
+  
+  static let sharedInstance: CardListDataSource = {
+    let instance = CardListDataSource()
+    return instance
+  }()
+  
+  // MARK: - Properties
+  //
+  private var cardList:[Card] = []
+  
+  
+  // MARK: - Initialization
+  //
+  override init() {
+    super.init()
+    populateData()
+  }
+  
+  
+  // MARK:- Populate Data from plist
+  //
+  func populateData() {
+    if let path = Bundle.main.path(forResource: "CardList", ofType: "plist") {
+      if let dictArray = NSArray(contentsOfFile: path) {
+        for item in dictArray {
+          if let dict = item as? NSDictionary {
+            if let data = Card(dictionary: dict) {
+              cardList.append(data)
             }
+          }
         }
+      }
     }
-    
-    
-    // MARK:-Return Cards
-    //
-    func  fetchCardList() -> [Card] {
-        return cardList
-    }
-    
-    
-    // MARK : - Get Number Of Cards
-    //
-    func numbeOfCards() -> Int {
-        return cardList.count
-    }
+  }
+  
+  
+  // MARK:-Return Cards
+  //
+  func  fetchCardList() -> [Card] {
+    return cardList
+  }
+  
+  
+  // MARK : - Get Number Of Cards
+  //
+  func numbeOfCards() -> Int {
+    return cardList.count
+  }
 }
