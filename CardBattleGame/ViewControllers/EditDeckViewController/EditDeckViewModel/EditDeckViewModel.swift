8b00e7b0bc7137ba93352928c6d2d806ed793562
SAHNAWAJ BISWAS <md.sahnawajbiswas@softvision.com>
2017-06-29 12:56:28 +0530

    Move Fetch Result Controller delegates to edit deck view model and notify changes in storage via delegates to EditDeckViewController.

--- CardBattleGame/ViewControllers/EditDeckViewController/EditDeckViewModel/EditDeckViewModel.swift
+++ CardBattleGame/ViewControllers/EditDeckViewController/EditDeckViewModel/EditDeckViewModel.swift
@@ -14,9 +14,65 @@ enum CardAddRemoveType {
   case delete
 }
 
+protocol EditDeckViewModelDelegate: class {
+  func deckCardEntityWillChangeContent()
+  func deckCardEntity(didChangeObjectAt indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?)
+  func deckCardEntityDidChangeContent()
+}
+
 /// Edit Deck View Model(Follow MVVM pattern): Handle all the business logics for EditDeckViewController
 class EditDeckViewModel: NSObject {
   
+  private let coreDataManager = CoreDataStackManager.sharedInstance
+  private let cardListDataSource = CardListDataSource.sharedInstance
+  
+  weak var delegate : EditDeckViewModelDelegate?
+  
+  // NSFetchedResultsController to updated uitableview if there is any changes in the Coredata storage
+  var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
+  
+  override init() {
+    super.init()
+    initializeFetchedResultsController()
+  }
+  
+  //Creating a Fetched Results Controller
+  private func initializeFetchedResultsController() {
+    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "DeckCard")
+    let departmentSort = NSSortDescriptor(key: "id", ascending: true)
+    request.sortDescriptors = [departmentSort]
+    let moc = coreDataManager.managedObjContext()
+    fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+    fetchedResultsController.delegate = self
+    
+    do {
+      
+      try fetchedResultsController.performFetch()
+      
+    } catch {
+      
+      fatalError("Failed to initialize FetchedResultsController: \(error)")
+    }
+  }
+  
+  /// Fetch Object at indexpath from Core Data Storage
+  ///
+  /// - Parameter indexPath: Index path of the object
+  /// - Returns: object from core data
+  func fetchObject(at indexPath: IndexPath) -> Any{
+    return fetchedResultsController.object(at: IndexPath(row: indexPath.row, section: 0))
+  }
+  
+  /// Number of cards available in the storage
+  ///
+  /// - Returns: Total Number of cards
+  func numberOfCardAdded() -> Int {
+    guard let sections = fetchedResultsController.sections else {
+      fatalError("No sections in fetchedResultsController")
+    }
+    let sectionInfo = sections[0]
+    return sectionInfo.numberOfObjects
+  }
   
   /// Add or Remove Card from the storage
   ///
@@ -24,29 +80,30 @@ class EditDeckViewModel: NSObject {
   ///   - card: Card object
   ///   - type: Add or Remove Activity Type
   func addRemove(card: Card, type: CardAddRemoveType) {
-    let fetchRequest = NSFetchRequest<Cards>()
+    let fetchRequest = NSFetchRequest<DeckCard>()
     fetchRequest.predicate = NSPredicate(format: "id == %d", card.id)
     
     // Create Entity Description
-    let entityDescription = NSEntityDescription.entity(forEntityName: "Cards", in: CoreDataStackManager.sharedInstance.managedObjContext())
+    let entityDescription = NSEntityDescription.entity(forEntityName: "DeckCard", in: coreDataManager.managedObjContext())
     
     // Configure Fetch Request
     fetchRequest.entity = entityDescription
     
     do {
-      let managedObjectContext = CoreDataStackManager.sharedInstance.managedObjContext()
+      let managedObjectContext = coreDataManager.managedObjContext()
       
       let result = try managedObjectContext.fetch(fetchRequest)
       
       switch  type{
       case .add: // Add Card If not available
         if result.count == 0{
-          let newItem: Cards = NSEntityDescription.insertNewObject(forEntityName: "Cards", into: managedObjectContext) as! Cards
-          newItem.attack = card.attack
-          newItem.battlepoint = card.battlepoint
-          newItem.health = card.health
-          newItem.id = card.id
-          newItem.name = card.name
+          if let newItem: DeckCard = NSEntityDescription.insertNewObject(forEntityName: "DeckCard", into: managedObjectContext) as? DeckCard{
+            newItem.attack = card.attack
+            newItem.battlepoint = card.battlepoint
+            newItem.health = card.health
+            newItem.id = card.id
+            newItem.name = card.name
+          }
         }
       case .delete: // Remove Card
         let resultData = result
@@ -54,29 +111,29 @@ class EditDeckViewModel: NSObject {
           managedObjectContext.delete(object)
         }
       }
-      CoreDataStackManager.sharedInstance.saveContext()
+      coreDataManager.saveContext()
       
     } catch {
       CBGErrorHandler.handle(error: .failedManagedObjectFetchRequest)
     }
   }
-    
+  
   /// Check if the card is available in the Persistance Storage
   ///
   /// - Parameter card: Card
   /// - Returns: True if card is available
   func checkCardState(card: Card) -> Bool {
-    let fetchRequest = NSFetchRequest<Cards>()
+    let fetchRequest = NSFetchRequest<DeckCard>()
     fetchRequest.predicate = NSPredicate(format: "id == %d", card.id)
     
     // Create Entity Description
-    let entityDescription = NSEntityDescription.entity(forEntityName: "Cards", in: CoreDataStackManager.sharedInstance.managedObjContext())
+    let entityDescription = NSEntityDescription.entity(forEntityName: "DeckCard", in: coreDataManager.managedObjContext())
     
     // Configure Fetch Request
     fetchRequest.entity = entityDescription
     
     do {
-      let managedObjectContext = CoreDataStackManager.sharedInstance.managedObjContext()
+      let managedObjectContext = coreDataManager.managedObjContext()
       
       let result = try managedObjectContext.fetch(fetchRequest)
       
@@ -89,10 +146,29 @@ class EditDeckViewModel: NSObject {
   }
   
   func fetchCardFromPlist() -> [Card] {
-    return CardListDataSource.sharedInstance.fetchCardList()
+    return cardListDataSource.fetchCardList()
   }
   
   func numberOfCardInPlist() -> Int {
-    return CardListDataSource.sharedInstance.numbeOfCards()
+    return cardListDataSource.numbeOfCards()
   }
+  
+}
+
+
+// MARK: - NSFetchedResultsControllerDelegate
+extension EditDeckViewModel: NSFetchedResultsControllerDelegate {
+  
+  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    self.delegate?.deckCardEntityWillChangeContent()
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    self.delegate?.deckCardEntity(didChangeObjectAt: indexPath, for: type, newIndexPath: newIndexPath)
+  }
+  
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    self.delegate?.deckCardEntityDidChangeContent()
+  }
+  
 }
